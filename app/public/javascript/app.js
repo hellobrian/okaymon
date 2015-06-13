20b7baf70f64e968610fdcf6a2a6799684b9ba69
thisisbrianhan <bthan@us.ibm.com>
2015-06-12 22:47:56 -0500

    Move routes into app.js Remove hashes from urls

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -1,1 +1,22 @@
 angular.module('Okaymon', ['ngRoute']);
+
+
+// ROUTES
+
+angular.module('Okaymon')
+.config(function($routeProvider, $locationProvider){
+  
+  $routeProvider
+  .when('/', {
+    templateUrl: '../templates/pokemon/index.html'
+  })
+  .when('/pokemon', {
+    templateUrl: '../templates/pokemon/index.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+
+  $locationProvider.html5mode(true);
+
+});
