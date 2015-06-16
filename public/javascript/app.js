bf7e07bbc5be48913eaeb290a105153ef6953e27
thisisbrianhan <bthan@us.ibm.com>
2015-06-15 19:15:33 -0500

    Use factory to return all pokemon

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,8 +1,27 @@
-angular.module('okaymon', ['ngRoute'])
-.config(function($routeProvider) {
+var app = angular.module('okaymon', ['ngRoute'])
+
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'templates/pokemon-index.html'
     })
     ;
+}]);
+
+app.controller('mainController', function($http, Pokemon) {
+  var _this = this;
+  _this.message = 'ima main controller';
+  
+  Pokemon.all()
+  .success(function(data) {
+    _this.pokemon = data;
+  });
 });
+
+app.factory('Pokemon', function PokemonFactory($http) {
+  return {
+    all: function() {
+      return $http({ method: 'GET', url: '/api/pokemon' });
+    }
+  }
+})
