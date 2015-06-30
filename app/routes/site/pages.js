c40a8f62ac7bdb34f90a9b0a2b3030fc887cfcfe
thisisbrianhan <bthan@us.ibm.com>
2015-06-30 15:10:32 -0500

    Add aileron fonts

--- app/routes/site/pages.js
+++ app/routes/site/pages.js
@@ -5,7 +5,8 @@ var express = require('express'),
 pageRouter.route('/').get(function (req, res) {
   var apiUrl = 'http://okaymon.mybluemix.net/api/'
   var generation1 = apiUrl + 'generation/1';
-  request(generation1, function(request, response, body) {
+  var pokemon1 = apiUrl + 'pokemon/1'
+  request(pokemon1, function(request, response, body) {
     var pokemon = JSON.parse(body);
     res.render('index', {layout: 'main-template', pokemon: pokemon });    
   })
