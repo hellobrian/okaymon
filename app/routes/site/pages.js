df71759157bbf4808b7204a92e93ea09ba25e198
thisisbrianhan <bthan@us.ibm.com>
2015-07-01 01:06:17 -0500

    Move index page css to main-template; remove request to API

--- app/routes/site/pages.js
+++ app/routes/site/pages.js
@@ -3,12 +3,7 @@ var express = require('express'),
     request = require('request');
 
 pageRouter.route('/').get(function (req, res) {
-  var apiUrl = 'http://okaymon.mybluemix.net/api/'
-  var pokemon1 = apiUrl + 'pokemon/1'
-  request(pokemon1, function(request, response, body) {
-    var pokemon = JSON.parse(body);
-    res.render('index', {layout: 'main-template', pokemon: pokemon });    
-  })
+  res.render('index', {layout: 'main-template'});    
 });
 
 module.exports = pageRouter;
